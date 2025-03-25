20aa0d527e5de1f50cbcc22264629d61355a39e2
xlmr-ux <82855367+xlmr-ux@users.noreply.github.com>
2025-03-25 16:46:55 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.1.108',
-    port: 3000
+    host: '192.168.1.108', // Your local IP
+    port: 3000, // Optional: specify a port
+    strictPort: true // Optional: prevent fallback to another port
   }
 });
