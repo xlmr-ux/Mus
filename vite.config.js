fb8ff01bd451d62e3a29e33d75f147ccac29e43a
xlmr-ux <82855367+xlmr-ux@users.noreply.github.com>
2025-03-25 16:25:15 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
+  server: {
+    host: '192.168.1.108', // Your local IP
+    port: 3000, // Optional: specify a port
+  }
 });
